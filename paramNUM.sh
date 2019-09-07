da12c7681f4a6fcbd91a65c04ffc78b192fe92dd
Rashid-kashmiri <52603814+Rashid-kashmiri@users.noreply.github.com>
2019-09-07 21:59:27 +0530

    Update paramNUM.sh

--- paramNUM.sh
+++ paramNUM.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "this is an example of script"
 echo "this is an example of script2"
-echo "7c022cdc5a8287323a32c393cfe75fe68a4265"
+echo "7c022cdc5a8287323a32c393cfe75fe68a4"
 echo "this is Running in a bulit step"
 echo "this is a Parameter $PASSED"
 NUM=$(($FIRSTNUMBER+$SECONDNUMBER))
