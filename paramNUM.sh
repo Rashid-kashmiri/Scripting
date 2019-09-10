46a827dda58b2ae48b576bc7403e3104d2c86748
Rashid-kashmiri <52603814+Rashid-kashmiri@users.noreply.github.com>
2019-09-10 14:37:49 +0530

    Update paramNUM.sh

--- paramNUM.sh
+++ paramNUM.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "this is an example of script"
 echo "this is an example of script2"
-echo "SUMIT123"
+echo "Changes made for webhook check"
 echo "this is Running in a bulit step"
 echo "this is a Parameter $PASSED"
 NUM=$(($FIRSTNUMBER+$SECONDNUMBER))
